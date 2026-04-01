814cca2a7b0976ec92b378d8228b2336046d9d28
hamdanshafqat78 <hamdan.shafqat78@gmail.com>
2026-04-02 00:46:14 +0500

    till lecture 9 done

--- web labs/js Practice/lecture 8/binding.js
+++ web labs/js Practice/lecture 8/binding.js
@@ -1,19 +1,39 @@
-window.onload = function () {
-    console.log("window loaded");
-    var btn = this.document.getElementById("btnAdd");
-    btn.onclick = bindingFunction;
-}
 
 
 
 function bindingFunction() {
     console.log("from binding function");
-    var fieldText = document.getElementById("textField").value;
-    var ul = document.getElementById("ulist");
-    var litem = document.createElement("li");
-    litem.textContent = fieldText;
-    ul.appendChild(litem);
-    document.getElementById("textField").value = "";
+    var fieldText = document.getElementById("textField");
+    var txt_node = document.createTextNode(fieldText.value);
+    var ul = document.getElementById("ul_list");
+    var new_li_item = document.createElement("li");
+    new_li_item.appendChild(txt_node);
+
+    var btn = document.createElement("button");
+    btn.innerText= "delete";
+    btn.onclick = handleDelete;
+    new_li_item.appendChild(btn);
 
 
+
+
+    ul.appendChild(new_li_item);
+    
+
+
+  
+
+    fieldText.value =  "";
+
+}
+function handleDelete(e) {
+    var tag = e.target;
+    var li = tag.parentNode;  
+    li.parentNode.removeChild(li);
+}
+
+window.onload = function () {
+    console.log("window loaded");
+    var btn = document.getElementById("btnAdd");
+    btn.onclick = bindingFunction;
 }
---
+++ web labs/js Practice/lecture 9/jspracric.js
@@ -0,0 +1,20 @@
+$(function () {
+    $("#addbtn").click(handlebtnclick);
+    // $("#ul_list li").click(delete_li);
+    $("#ul_list").on("click", "li", delete_li);
+
+});
+function handlebtnclick() {
+    //alert("btn clicked ");
+    var new_value = $("#new_input").val();
+    if (!new_value) {
+        $("#new_input").addClass("error");
+        return;
+    }
+    $("#ul_list").append("<li>" + new_value + "</li>");
+    $("#new_input").val("");
+    //$("#ul_list li").click(delete_li);
+}
+function delete_li() {
+    $(this).remove();
+}
