e84fa59bc3a0d10521dfc902a9dfd663c02befef
hamdanshafqat78 <hamdan.shafqat78@gmail.com>
2026-04-03 00:41:50 +0500

    lectueres 13 done

--- web labs/js Practice/lecture 13/jqcrud.js
+++ web labs/js Practice/lecture 13/jqcrud.js
@@ -1,10 +1,36 @@
 $(function () {
     loadRecepie();
+    $("#recepies").on("click", ".delete", handleDelete);
 });
+function handleDelete() {
+    console.log("delete clicked");
+    var btn = $(this);
+    var parnentDiv = btn.closest(".card");
+    var id = parnentDiv.attr("data-id");
+    console.log(id);
+
+    $.ajax({
+        url: "https://usmanlive.com/wp-json/api/stories/" + id,
+        method: "DELETE",
+        success: function (response) {
+            console.log(response);
+            loadRecepie();
+
+        }
+    });
+
+
+}
+
 function loadRecepie() {
     $.ajax({
         url: "https://usmanlive.com/wp-json/api/stories",
         method: "GET",
+        error: function (error) {
+            var rec = $("#recepies");
+            rec.empty();
+            rec.append("hehehehheh rasty main gum ho gye ")
+        },
         success: function (response) {
             console.log(response);
             var rec = $("#recepies");
@@ -12,7 +38,13 @@ function loadRecepie() {
             for (var i = 0; i < response.length; i++) {
                 //rec.append("<div><h3>" + response[i].title + "</h3></div>");
                 //rec.append("<p>" + response[i].content + "</p>");
-                rec.append(`<div class="card"><h3>${response[i].title}</h3><p>${response[i].content}<button class ="btn btn-danger" style="float: right;">delete</button></p></div>`);
+                rec.append(`<div class="card" data-id="${response[i].id}">
+                                <h3>${response[i].title}</h3>
+                                <p>${response[i].content}
+                                    <button class ="delete btn btn-danger btn-sm float-right" >delete</button>
+                                    <button class ="edit btn btn-warning btn-sm float-right" >edit</button>
+                                </p>
+                            </div>`);
 
 
             }
